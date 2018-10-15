98a5e818332046ebab6f8edab7aa2417245ff8f9
Mahmoud Barake <mahmoudbarake95@gmail.com>
2018-10-15 00:39:06 -0400

    fixes bugs in UserControllerIntegrationTest

--- src/test/java/com/h2rd/refactoring/integration/UserControllerIntegrationTest.java
+++ src/test/java/com/h2rd/refactoring/integration/UserControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -12,8 +13,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.junit.After;
 import org.junit.Test;
@@ -26,10 +25,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import com.h2rd.refactoring.Application;
-import com.h2rd.refactoring.exception.BadRequestException;
-import com.h2rd.refactoring.model.Role;
 import com.h2rd.refactoring.model.User;
 import com.h2rd.refactoring.repository.UserRepository;
+import com.h2rd.refactoring.util.TestUtil;
 
 
 @RunWith(SpringRunner.class)
@@ -49,129 +47,135 @@ public class UserControllerIntegrationTest {
     }
 
     @Test
-    public void givenUsers_whenGetUsers_thenStatus200() throws Exception {
-        User user1 = createUser("john","john@gmail.com","student");
+    public void givenUsers_whenGetUsers_thenReturnUsersJsonArray() throws Exception {
+        //given
+        User user1 = TestUtil.createUser("john","john@gmail.com","student");
+        User user2 = TestUtil.createUser("peter","peter@gmail.com","Teacher");
         userRepository.saveAndFlush(user1);
-
-        //get a particular user
-        mvc.perform(get("/users/john@gmail.com").contentType(MediaType.APPLICATION_JSON))
-        .andDo(print())
-        .andExpect(status().isOk())
-        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.name", is("john")));
-
-        User user2 = createUser("peter","peter@gmail.com","Teacher");
         userRepository.saveAndFlush(user2);
-
-        //get all users
+        //when
         mvc.perform(get("/users").contentType(MediaType.APPLICATION_JSON))
         .andDo(print())
-        .andExpect(status().isOk())
         .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+        //then
+        .andExpect(status().isOk())
         .andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(2))))
         .andExpect(jsonPath("$[0].name", is("john")))
         .andExpect(jsonPath("$[1].name", is("peter")));
     }
 
     @Test
-    public void whenValidInput_thenCreateUser() throws IOException, Exception {
+    public void givenUser_whenGetUser_thenReturnUserJsonArray() throws Exception {
+        //given
+        User user1 = TestUtil.createUser("john","john@gmail.com","student");
+        userRepository.saveAndFlush(user1);
+        //when
+        mvc.perform(get("/users/john@gmail.com").contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+        //then
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.name", is("john")));
+    }
+
+    @Test
+    public void givenValidInput_whenCreateUser_thenUserCreatedSucessfully() throws IOException, Exception {
+        //given
         String jsonContent = "{\"name\": \"Mahmoud\",\"email\": \"mahmoud@gmail.com\",\"roles\": [\"student\"]}";
+        //when
         mvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON).content(jsonContent));
+        //then
         List<User> foundUsers = userRepository.findAll();
         assertThat(foundUsers).extracting(User::getName).containsOnly("Mahmoud");
     }
 
     @Test
-    public void whenInValidInput_thenDoNotCreateUser() throws IOException, Exception {
-        //invalid email
+    public void givenInValidInput_whenCreateUser_thenUserNotCreated() throws IOException, Exception {
+        //given (invalid email)
         String jsonContentInvalidEmail = "{\"name\": \"Mahmoud\",\"email\": \"mahmoudbarake95gmail.com\",\"roles\": [\"student\"]}";
+        //when
         mvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON).content(jsonContentInvalidEmail));
+        //then
         List<User> foundUsersFromInvalidEmail = userRepository.findAll();
         assertThat(foundUsersFromInvalidEmail.isEmpty());
 
-        //empty name
+        //given (empty name)
         String jsonContentEmptyName = "{\"name\": \"\",\"email\": \"mahmoudbarake@95gmail.com\",\"roles\": [\"student\"]}";
+        //when
         mvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON).content(jsonContentEmptyName));
+        //then
         List<User> foundUsersFromEmptyName = userRepository.findAll();
         assertThat(foundUsersFromEmptyName.isEmpty());
 
-        //empty role
+        //given (empty role)
         String jsonContentEmptyRole = "{\"name\": \"Mahmoud\",\"email\": \"mahmoudbarake95gmail.com\",\"roles\": [\"\"]}";
+        //when
         mvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON).content(jsonContentEmptyRole));
+        //then
         List<User> foundUsersFromEmptyRole = userRepository.findAll();
         assertThat(foundUsersFromEmptyRole.isEmpty());
     }
 
     @Test
-    public void whenValidInput_thenUpdateUser() throws IOException, Exception {
-        User user1 = createUser("john","john@gmail.com","student");
+    public void givenValidInput_whenUpdateUser_thenUserUpdatedSucessfully() throws IOException, Exception {
+        //given
+        User user1 = TestUtil.createUser("john","john@gmail.com","student");
         userRepository.saveAndFlush(user1);
-
-        //update name and role
         String jsonContent = "{\"name\": \"john smith\",\"email\": \"john@gmail.com\",\"roles\": [\"teacher\"]}";
+        //when (update name and role)
         mvc.perform(put("/users/john@gmail.com").contentType(MediaType.APPLICATION_JSON).content(jsonContent));
+        //then
         List<User> foundUsers = userRepository.findAll();
         assertThat(foundUsers).extracting(User::getName).containsOnly("john smith");
     }
 
     @Test
-    public void whenInValidInput_thenDoNotUpdateUser() throws IOException, Exception {
-        User user1 = createUser("john","john@gmail.com","student");
+    public void givenInValidInput_whenUpdateUser_thenUserNotUpdated() throws IOException, Exception {
+        //given (different email)
+        User user1 = TestUtil.createUser("john","john@gmail.com","student");
         userRepository.saveAndFlush(user1);
-
-        //cannot update email
         String jsonContentUpdateEmail = "{\"name\": \"john smith\",\"email\": \"johngmail.com\",\"roles\": [\"teacher\"]}";
+        //when 
         mvc.perform(put("/users/john@gmail.com").contentType(MediaType.APPLICATION_JSON).content(jsonContentUpdateEmail));
+        //then
         List<User> foundUsersUpdateEmail = userRepository.findAll();
-        assertThat(foundUsersUpdateEmail).extracting(User::getEmail).containsOnly("john@gmail.com");//email remains john@gmail.com
+        assertThat(foundUsersUpdateEmail).extracting(User::getEmail).containsOnly("john@gmail.com");//cannot update email, email remains john@gmail.com
 
-        //empty name
+        //given (empty name)
         String jsonContentEmptyName = "{\"name\": \"\",\"email\": \"john@gmail.com\",\"roles\": [\"teacher\"]}";
+        //when
         mvc.perform(put("/users/john@gmail.com").contentType(MediaType.APPLICATION_JSON).content(jsonContentEmptyName));
+        //then
         List<User> foundUsersEmptyName = userRepository.findAll();
         assertThat(foundUsersEmptyName).extracting(User::getName).containsOnly("john");//name remains john
-
-        //empty role
-//        String jsonContentEmptyRole = "{\"name\": \"john smith\",\"email\": \"john@gmail.com\",\"roles\": [\"\"]}";
-//        mvc.perform(put("/users/john@gmail.com").contentType(MediaType.APPLICATION_JSON).content(jsonContentEmptyRole));
-//        List<User> foundUsersEmptyRole = userRepository.findAll();
-//        assertThat(foundUsersEmptyRole).extracting(User::getRoles).containsOnly("teacher");//name remains john
-
-
     }
 
     @Test
-    public void whenValidIdentifier_thenDeleteUser() throws IOException, Exception {
-        User user1 = createUser("john","john@gmail.com","student");
+    public void givenValidEmail_whenDeleteUser_thenUserDeleted() throws IOException, Exception {
+        //given
+        User user1 = TestUtil.createUser("john","john@gmail.com","student");
         userRepository.saveAndFlush(user1);
-
-        mvc.perform(get("/users/john@gmail.com").contentType(MediaType.APPLICATION_JSON))
+        //when
+        mvc.perform(delete("/users/john@gmail.com").contentType(MediaType.TEXT_PLAIN))
         .andDo(print())
-        .andExpect(status().isOk())
-        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-
+        //then
+        .andExpect(status().isOk());
         List<User> foundUsersAfterDelete = userRepository.findAll();
         assertThat(foundUsersAfterDelete.isEmpty());
     }
 
     @Test
-    public void whenInValidIdentifier_thenDoNotDeleteUser() throws IOException, Exception {
-        User user1 = createUser("john","john@gmail.com","student");
+    public void givenInValidEmail_whenDeleteUser_thenUserNotDeleted() throws IOException, Exception {
+        //given
+        User user1 = TestUtil.createUser("john","john@gmail.com","student");
         userRepository.saveAndFlush(user1);
-
-        //invalid email
-        mvc.perform(get("/users/johngmail.com").contentType(MediaType.APPLICATION_JSON))
+        //when (invalid email)
+        mvc.perform(delete("/users/johngmail.com").contentType(MediaType.TEXT_PLAIN))
         .andDo(print())
+        //then
         .andExpect(status().isNotFound());
-
         List<User> foundUsersAfterDelete = userRepository.findAll();
         assertThat(foundUsersAfterDelete.size()==1);
-    }
-
-
-    private User createUser(String name, String email, String roleName){
-        return new User(name,email, new ArrayList<Role>(
-                Arrays.asList(new Role(roleName))));
     }
 
 }
